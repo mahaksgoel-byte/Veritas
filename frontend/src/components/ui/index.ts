4de14195143e076b3c4c5fb0b8c7e9d39e706ba4
Mahak Goel <mahak.s.goel@gmail.com>
2026-02-20 04:52:16 +0530

    Clear repository

--- frontend/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Section } from './Section';
-export { Grid } from './Grid';
-export { Textarea } from './Textarea';
-export { RoleCard } from './RoleCard';
